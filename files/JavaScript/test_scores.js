afa29938bd93c47a5adcd28eb91b35b438b7bd11
Ciroh1 <111922074+Ciroh1@users.noreply.github.com>
2022-12-03 21:21:00 -0600

    m

--- files/JavaScript/test_scores.js
+++ files/JavaScript/test_scores.js
@@ -9,7 +9,7 @@ total += score2;
 const score3 = parseInt(prompt("Enter test score"));
 total += score3;
 
-const average = parseInt(total/3);
+const average = parseInt(total / 3);
 
 const html1 = `<p>Score 1 = ${score1}</p>
               <p>Score 2 = ${score2}</p>
