8206fecee2cc0ab77079ed8aa2b66a5193f40a9c
Ciroh1 <111922074+Ciroh1@users.noreply.github.com>
2022-12-04 08:09:26 -0600

    m

--- JavaScript/test_scores.js
+++ JavaScript/test_scores.js
@@ -1,7 +1,7 @@
 let total = 0;
 
 const score1 = parseInt(prompt("Enter test score"));
-total += score1;        
+total += score1;
 
 const score2 = parseInt(prompt("Enter test score"));
 total += score2;
@@ -9,11 +9,10 @@ total += score2;
 const score3 = parseInt(prompt("Enter test score"));
 total += score3;
 
-const average = parseInt(total/3);
+const average = parseInt(total / 3);
 
 const html1 = `<p>Score 1 = ${score1}</p>
               <p>Score 2 = ${score2}</p>
               <p>Score 3 = ${score3}</p>
               <p>Average score = ${average}</p>`;
 document.write(html1);
-
